282dd6f3db207146739c2292cbd3f8ace3fc8391
Marzieh-Mi <marzi.mohrimm@gmail.com>
2026-03-12 00:28:39 +0330

    Create Gene-co-expression-network-analysis.R

---
+++ Gene-co-expression-network-analysis.R
@@ -0,0 +1,20 @@
+# Gene-co-expression-network-analysis
+This repository includes scripts and workflows for analyzing the gene co-expression networks.
+
+## Method
+Weighted Gene Co-expression Network Analysis (WGCNA)
+
+Reference:
+Langfelder, P., & Horvath, S. (2008). WGCNA: an R package for weighted correlation network analysis. 
+
+## Analysis Workflow
+1. Import gene expression data
+2. Construct co-expression modules
+3. Visualization
+
+## Tools
+- R
+- WGCNA package
+
+## Related Repositories
+- Downstream-functional-analysis (GO, promoter, miRNA)
